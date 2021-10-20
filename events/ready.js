b3d4afd962aa5febab2e5e5aa6a8284fdd91fb7f
Maks <max.centeno64@gmail.com>
2021-10-20 17:21:55 +1100

    added r/unixporn auto poster

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,24 @@
 const client = require("../index");
-
+const chalk = require("chalk")
+const Discord = require("discord.js")
 client.on("ready", () =>{
-    console.log(`${client.user.tag} is ready`)
+    console.log(chalk.green(`${client.user.tag} is ready`))
     client.user.setActivity({
         name: "Xelph Linux",
         type: "PLAYING"
     })
+    const got = require('got')
+    const channel = client.channels.cache.get("900264447497633812");
+
+    setInterval(() => {
+        got('https://www.reddit.com/r/unixporn/hot/.json').then(res => {
+            let content = JSON.parse(res.body)
+            const embed = new Discord.MessageEmbed()
+                    .setTitle(content[0].data.children[0].data.title)
+                    .setImage(content[0].data.children[0].data.url)
+                    .setColor("RANDOM")
+                    .setFooter(`👍 ${content[0].data.children[0].data.ups} 👎 ${content[0].data.children[0].data.downs} | Comments : ${content[0].data.children[0].data.num_comments}`)
+            channel.send({embeds:[embed]})
+        })
+    }, 50000)
 });

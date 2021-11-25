b4729447f6b856c101339d2181c729a5870ae085
Maks <73048812+LinusBotTips@users.noreply.github.com>
2021-11-25 20:05:10 +1100

    remove reddit

--- events/ready.js
+++ events/ready.js
@@ -9,26 +9,6 @@ client.on("ready", () =>{
         name: "Xelph Linux",
         type: "PLAYING"
     })
-    const channel = client.channels.cache.get("900264447497633812");
-
-    const statschanel = client.channels.cache.get("900295611239235594")
-    setInterval(() => {
-        let url = `https://api.github.com/repos/xelphlinux/xelph-iso/releases/latest`;
-        axios.get(url).then((res) => { 
-            statschanel.setName(`${res.data.tag_name}: ${res.data.assets[0].download_count} downloads!`)
-        })
-    }, 900000)
-    setInterval(() => {
-        got('https://www.reddit.com/r/unixporn/random/.json').then(res => {
-            let content = JSON.parse(res.body)
-            const embed = new Discord.MessageEmbed()
-                    .setTitle(content[0].data.children[0].data.title)
-                    .setImage(content[0].data.children[0].data.url)
-                    .setColor("RANDOM")
-                    .setFooter(`👍 ${content[0].data.children[0].data.ups} 👎 ${content[0].data.children[0].data.downs} | Comments : ${content[0].data.children[0].data.num_comments}`)
-            channel.send({embeds:[embed]})
-        })
-    }, 900000)
 
 
 });
